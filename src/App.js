0a400f3b0b1c162cf399cb6f8dcbe33a15fbf174
Chyph <gustavo.gigante.s@gmail.com>
2019-11-20 23:02:48 +0000

    Form attemp - failed

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ import './App.css';
 
 function App() {
   const [budget, setBudget] = useState('109,60');
+
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  
   const [purchases, setPurchase] = useState([
     { purchase: 'Padaria Pastelaria', amount: '2,40', date: '20-11-2019' },
     { purchase: 'Restaurante', amount: '15,00', date: '17-11-2019' },
     { purchase: 'Mercearia Supermercado', amount: '80,00', date: '20-11-2019' },
   ]);
 
-  const addPurchase = () => { // Add a new purchase to purchases array
-    const newPurchase = { purchase: 'Pneus Auto', amount: '150,00', date: '04-06-2019' };
-    setPurchase(oldPurchases => [...oldPurchases, newPurchase]);
+  const addPurchase = (event) => { // Add a new purchase to purchases array
+    /*const newPurchase = { purchase: 'Pneus Auto', amount: '150,00', date: '04-06-2019' };
+    setPurchase(oldPurchases => [...oldPurchases, newPurchase]);*/
+    event.preventDefault();
+    console.log({ item, amount, date });
   }
 
   return (
@@ -34,7 +41,17 @@ function App() {
         <Movements purchase={ purchase.purchase } amount={ purchase.amount } date={ purchase.date }/>
       ))}
 
-      <button onClick={ addPurchase }>Add New</button>
+      <form onSubmit={ addPurchase }>
+        <label>
+          Purchase:
+          <input type="text" name="item" value={item} onChange={ e => setPurchase(e.target.value) }/>
+        </label>
+        Amount:
+        <input type="text" name="amount" value={amount} onChange={ e => setAmount(e.target.value) } />
+        Date:
+        <input type="text" name="date" value={date} onChange={ e => setDate(e.target.value) } />
+        <input type="submit" value="Enter" />
+      </form>
 
     </div>
   </div>
